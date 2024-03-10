a152cef9790a73fc5262057354e9fd59324844ea
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-10 19:18:38 +0900

    feat: create routing routes for home, one and two player components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OnePlayerComponent } from './games/one-player/one-player.component';
+import { TwoPlayerComponent } from './games/two-player/two-player.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '1player', component: OnePlayerComponent },
+  { path: '2player', component: TwoPlayerComponent },
+];
