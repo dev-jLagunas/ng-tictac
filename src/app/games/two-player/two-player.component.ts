a1273c0e8f65f8a96f6a2d9760e4b2a0081b243d
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-12 13:22:48 +0900

    feat(two player component): complete two player component html, scss styles, and typescript logic.

--- src/app/games/two-player/two-player.component.ts
+++ src/app/games/two-player/two-player.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { Cell } from '../../types/cell.type';
+import { GameplayService } from '../../service/gameplay.service';
 @Component({
   selector: 'app-two-player',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgClass],
   templateUrl: './two-player.component.html',
-  styleUrl: './two-player.component.scss'
+  styleUrl: './two-player.component.scss',
 })
 export class TwoPlayerComponent {
+  constructor(public gameplayService: GameplayService) {}
 
+  get board(): Cell[] {
+    return this.gameplayService.getBoard();
+  }
+
+  get activePlayer(): 'X' | 'O' {
+    return this.gameplayService.getActivePlayer();
+  }
+
+  updateCell(cell: Cell): void {
+    this.gameplayService.updateCell(cell);
+  }
+
+  resetGame(): void {
+    this.gameplayService.resetGame();
+  }
 }
